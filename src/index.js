1cac570b593f78b88d6c73761904512956c662e8
Lheanarda <alexanderleo0499@gmail.com>
2023-10-30 19:17:31 +0700

    add loading screen

--- src/index.js
+++ src/index.js
@@ -3,15 +3,25 @@ import Effect from "./class/Effect";
 import { PRINTED_TEXT, TOTAL_IMAGES } from "./constants";
 import { loadImage } from "./utils";
 
+const containerLoading = document.getElementById('container-loading')
+const loadingProgress = document.getElementById('loading-progress')
 
-window.addEventListener('load',async ()=>{
+function removeLoadingPage(){
+    containerLoading.style.display = 'none'
+}
+
+async function initCanvasPage(){
     const effect = new Effect()
 
+    let progress = 0
+    let progressPerPhoto = 100/TOTAL_IMAGES
     for(let i = 1; i<= TOTAL_IMAGES; i++){
         await loadImage(`/assets/photo${i}.jpg`)
+        progress += progressPerPhoto
+        loadingProgress.textContent = `${progress.toFixed(0)}%`
     }
 
-    
+    removeLoadingPage()
 
 
     function animate(){
@@ -29,8 +39,7 @@ window.addEventListener('load',async ()=>{
         effect.resize()
         effect.wrapText(PRINTED_TEXT)
     })
-
-})
-
+}
 
 
+initCanvasPage()
