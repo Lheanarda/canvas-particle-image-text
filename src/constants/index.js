e8203210b0f0368107559ba84361a3482f546e12
alexanderleotvlk <62197809+Lheanarda@users.noreply.github.com>
2023-12-06 02:54:18 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const FONT_FAMILY = 'Helvetica'
 // export const FONT_FAMILY = 'Permanent Marker'
-export const PRINTED_TEXT = 'TE AMO'
+export const PRINTED_TEXT = 'TE AMO MUCHO'
 export const GAP = 4
 
 export const TOTAL_IMAGES = 38
